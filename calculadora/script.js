b94474b012fe1bed8abd87096614fe4203661dfa
iurimega13 <iurimega13@gmail.com>
2021-03-28 23:39:29 -0300

    Transformando em número inteiro

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,6 +1,7 @@
 document.querySelector("#calcular").addEventListener("click", function () {
-    let number1 = document.querySelector("#number1").value;
-    let number2 = document.querySelector("#number2").value;
+    
+    let number1 = parseInt(document.querySelector("#number1").value);
+    let number2 = parseInt(document.querySelector("#number2").value);
     if (number1 > 0 && number2 > 0) {
         alert(number1 + number2);
     } else {

5e5ce393a4172927b6d6a8f61c6942f8fbaf7a5e
iurimega13 <iurimega13@gmail.com>
2021-03-28 23:35:13 -0300

    Adicionando condiçao para verificar se os valores são maiores que 0

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,5 +1,9 @@
 document.querySelector("#calcular").addEventListener("click", function () {
     let number1 = document.querySelector("number1").value;
     let number2 = document.querySelector("number2").value;
-
+    if (number1 > 0 && number2 > 0) {
+        alert(number1 + number2);
+    } else {
+        alert("Digite os valores para calcular");
+    };
 });

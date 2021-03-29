83319fab8956c41e9e282f6c342b10b6777b390a
iurimega13 <iurimega13@gmail.com>
2021-03-28 23:25:46 -0300

    Importando valores dos inputs

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,3 +1,5 @@
 document.querySelector("#calcular").addEventListener("click", function () {
-    alert('teste');
+    let number1 = document.querySelector("number1");
+    let number2 = document.querySelector("number2");
+    
 });

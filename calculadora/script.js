f51e4ee026922d16ac93eb42e74849fa3b9516a6
iurimega13 <iurimega13@gmail.com>
2021-03-28 23:27:48 -0300

    Colocando para pegar valores dos inputs corretamente

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,5 +1,5 @@
 document.querySelector("#calcular").addEventListener("click", function () {
-    let number1 = document.querySelector("number1");
-    let number2 = document.querySelector("number2");
-    
+    let number1 = document.querySelector("number1").value;
+    let number2 = document.querySelector("number2").value;
+
 });
